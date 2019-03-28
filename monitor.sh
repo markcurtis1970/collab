fb4d18b64be704ca553e7bcd67107268c50ff407
markcurtis1970 <mark.curtis.1970@gmail.com>
2019-03-28 23:08:50 +0000

    Fixes #3 and #4

--- monitor.sh
+++ monitor.sh
@@ -25,7 +25,6 @@ function useage {
     exit
 }
 
-
 function run_nodetool {
     echo ">>> $(date '+%Y-%m-%d_%H:%M:%S') ... running nodetool commands"
     for COMMAND in compactionstats describecluster failuredetector gcstats gossipinfo info netstats proxyhistograms status statusbinary statusgossip statushandoff 'tpstats --cores'
@@ -33,16 +32,21 @@ function run_nodetool {
         echo ">>> $(date '+%Y-%m-%d_%H:%M:%S') ... running nodetool $COMMAND" >> $LOG
         nodetool $COMMAND >> $LOG
     done
-    sleep $DELAY
+}
+
+function run_threaddump {
+    echo ">>> $(date '+%Y-%m-%d_%H:%M:%S') ... running thread dump"
+    echo ">>> $(date '+%Y-%m-%d_%H:%M:%S') ... running thread dump" >> $TD_LOG
+    sudo -u $CASSUSER jstack -l $CASSPID >> $TD_LOG
 }
 
 function run_cpu_ttop {
-   echo ">>> $(date '+%Y-%m-%d_%H:%M:%S') ... running ttop"
+   echo ">>> $(date '+%Y-%m-%d_%H:%M:%S') ... running ttop (CPU)"
    nodetool sjk ttop -o CPU -n 50 >> $TTOP_CPU_LOG &
 }
 
 function run_alloc_ttop {
-   echo ">>> $(date '+%Y-%m-%d_%H:%M:%S') ... running ttop"
+   echo ">>> $(date '+%Y-%m-%d_%H:%M:%S') ... running ttop (ALLOC)"
    nodetool sjk ttop -o ALLOC -n 50 >> $TTOP_ALLOC_LOG &
 }
 
@@ -60,24 +64,51 @@ if [ $# -ne 2 ]; then
     exit
 fi
 
+# Configuration 
+#
+# Set things like Cassandra PID here
+# for example you might use:
+#
+# $(cat /var/run/cassandra.pid)
+#
+# Also other things like file location,
+# date format, file names and such
 
 NODE=$(hostname -i)
 DATE=$(date '+%Y-%m-%d_%H:%M:%S')
-LOG="/tmp/$NODE-$DATE-monitor.out"
-TTOP_CPU_LOG="/tmp/$NODE-$DATE-monitor-ttop-cpu.out"
-TTOP_ALLOC_LOG="/tmp/$NODE-$DATE-monitor-ttop-alloc.out"
+MONDIR="/tmp"
 DELAY=$1
 COUNT=$2
+CASSPID=$(cat /run/dse/dse.pid)
+CASSUSER="cassandra"
+LOG="$MONDIR/$NODE-$DATE-monitor-$CASSPID.out"
+TD_LOG="$MONDIR/$NODE-$DATE-thread-dump-$CASSPID.out"
+TTOP_CPU_LOG="$MONDIR/$NODE-$DATE-monitor-ttop-cpu-$CASSPID.out"
+TTOP_ALLOC_LOG="$MONDIR/$NODE-$DATE-monitor-ttop-alloc-$CASSPID.out"
 
+# Main control
+#
+# This is where we call functions
+# to run commands to get diagnostics
+
+# Run the ttop commands first in the background
+# these will continue running until they are killed
+# if you CTRL+C this script you will have to kill
+# ttop manually
 echo "Running monitor script... please check nodetool sjk is not still running after with \"ps -ef | grep ttop\""
-
 run_cpu_ttop
 run_alloc_ttop
+
+# Execute loop to run regular commands
+# typically nodetool and thread dumps
 for loop in $(seq $COUNT)
 do
    run_nodetool
+   run_threaddump
    sleep $DELAY
 done
-kill_ttop
 
-echo "Exiting monitor script ... please find outputs in /tmp"
+# Clean up ttop processes and exit
+kill_ttop
+echo "Exiting monitor script ... please find outputs in $MONDIR"
+exit
